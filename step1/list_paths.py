21ee27fc8fe7273cf0435d41ae12946ac0d582ed
YOUR GITHUB USERNAME <YOUR_EMAIL_ADDRESS>
2017-04-17 14:12:58 -0700

    Rename folders and functionalized modifyLSP

---
+++ step1/list_paths.py
@@ -0,0 +1,20 @@
+import requests
+
+
+#### define ip of wae controller and port to connect on
+wae = "198.18.134.26"
+port = "7777"
+url = "http://" + wae + ":" + port + "/wae/network/collected/entities/tunnel/pcep/get-all-tunnels"
+
+
+##### set headers
+headers = {
+    'cache-control': "no-cache",
+    }
+
+
+#### send as get request
+response = requests.request("GET", url, headers=headers)
+
+####return tunnel paths
+print(response.text)
