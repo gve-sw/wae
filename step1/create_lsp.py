077bcb9ed42f0256a2ce6e5d1d45fb6ea3117cc6
chrabel <chrabel@cisco.com>
2017-04-17 14:25:25 -0700

    Functionalize Create and List

--- step1/create_lsp.py
+++ step1/create_lsp.py
@@ -1,28 +1,31 @@
 import requests
 
 #### define ip of wae controller and port to connect on
-wae="198.18.134.26"
-port="7777"
+# wae="198.18.134.26"
+# port="7777"
 
 ### set tunnel name source and dest
-tname="tunnel-1"
-tsource="sjc"
-tdest="rtp"
+# tname="tunnel-1"
+# tsource="sjc"
+# tdest="rtp"
+
+def createLSP(wae, port, tname, tsource, tdest):
 
 
-#### form request url
-url = "http://" + wae + ":" + port + "/wae/network/modeled/entities/tunnel/new/admit/basic"
 
-#### set json post
-payload = "{\r\n  \"teTunnel\" : {\r\n    \"name\" : \"%s\",\r\n    \"source\" : \"%s\",\r\n    \"destination\" : \"%s\",\r\n    \"pcep\" : false\r\n  }\r\n}"%(tname, tsource,tdest)
+    #### form request url
+    url = "http://" + wae + ":" + port + "/wae/network/modeled/entities/tunnel/new/admit/basic"
 
-### set headers
-headers = {
-    'content-type': "application/json",
-    'cache-control': "no-cache",
-    }
+    #### set json post
+    payload = "{\r\n  \"teTunnel\" : {\r\n    \"name\" : \"%s\",\r\n    \"source\" : \"%s\",\r\n    \"destination\" : \"%s\",\r\n    \"pcep\" : false\r\n  }\r\n}"%(tname, tsource,tdest)
 
-#### send post
-response = requests.request("POST", url, data=payload, headers=headers)
+    ### set headers
+    headers = {
+        'content-type': "application/json",
+        'cache-control': "no-cache",
+        }
 
-print(response.text)
+    #### send post
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    print(response.text)
--- step1/list_paths.py
+++ step1/list_paths.py
@@ -2,19 +2,23 @@ import requests
 
 
 #### define ip of wae controller and port to connect on
-wae = "198.18.134.26"
-port = "7777"
-url = "http://" + wae + ":" + port + "/wae/network/collected/entities/tunnel/pcep/get-all-tunnels"
+# wae = "198.18.134.26"
+# port = "7777"
+# url = "http://" + wae + ":" + port + "/wae/network/collected/entities/tunnel/pcep/get-all-tunnels"
 
 
-##### set headers
-headers = {
-    'cache-control': "no-cache",
-    }
+
+def listpaths(wae, port):
+    url = "http://" + wae + ":" + port + "/wae/network/collected/entities/tunnel/pcep/get-all-tunnels"
+
+    ##### set headers
+    headers = {
+        'cache-control': "no-cache",
+        }
 
 
-#### send as get request
-response = requests.request("GET", url, headers=headers)
+    #### send as get request
+    response = requests.request("GET", url, headers=headers)
 
-####return tunnel paths
-print(response.text)
+    ####return tunnel paths
+    print(response.text)
